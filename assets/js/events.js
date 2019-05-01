693f9dd0c4cd766a6755a0f4d1bc9d0d990df9fc
Jim Thoburn <jim@jimthoburn.com>
2019-04-30 22:10:52 -0700

    Add JavaScript for events and to make content changes

---
+++ assets/js/events.js
@@ -0,0 +1,334 @@
+/*
+Example Style
+
+<style>
+  .error .message,
+  .loading .message {
+    display: block !important;
+  }
+  .error .message {
+    color: red;
+  }
+</style>
+*/
+
+/*
+Example HTML
+
+<h1>Events for the City of Santa Monica in Palisades Park</h1>
+<p role="status" class="message" style="display: none;"></p>
+<ol id="event-list"></ol>
+*/
+
+(function() {
+  let list = document.querySelector('[data-events-locations]')
+  if (!list) return
+
+  let template = `
+    <li class="{{ className }}">
+      <details>
+        <summary>
+          <div class="summary">
+            <h3><span><abbr aria-label="{{ weekday }}">{{ weekdayAbbreviation }}</abbr><br /><abbr aria-label="{{ month }}">{{ monthAbbreviation }}</abbr> {{ day }}</span></h3>
+            <p>{{ title }}</p>
+            <p>{{ time }}</p>
+            <p>{{ location }}</p>
+          </div>
+          <div class="mask-behind-details"></div>
+        </summary>
+        <div class="details">
+          <h3>{{ title }}</h3>
+          <h4>{{ weekday }}, {{ month }} {{ day }}, {{ year }}</h4>
+          <p>{{ time }}</p>
+          <p>{{ description }}</p>
+
+          <dl>
+            <dt>Location</dt>
+            <dd>{{ location }}</dd>
+            <dd>{{ address }}</dd>
+            <dt>Categories</dt>
+            <dd>{{ categories }}</dd>
+            <dt>Ages</dt>
+            <dd>{{ ages }}</dd>
+            <dt>Contact<dt>
+            <dd>Santa Monica Cultural Affairs</dd>
+            <dd><a href="mailto:culture@smgov.net">culture@smgov.net</a></dd>
+            <dd>310-458-8350</dd>
+          </dl>
+
+          <p><a href="{{ url }}" target="_blank">Get more details about this event at {{ urlDomain }}</a></p>
+        </div>
+      </details>
+    </li>
+  `;
+
+  // Example requests for the City of Santa Monica events API
+
+  // Everything
+  // https://data.smgov.net/resource/tu9m-76aw.json
+
+  // Only locations and calendars
+  // https://data.smgov.net/resource/tu9m-76aw.json?$select=calendar,location&$order=calendar,location&$group=calendar,location
+
+  // showLoadingMessage()
+
+  // const EVENT_TYPES = []
+  // const EVENT_TYPES = [
+  //   "Art Event",
+  //   "Arts/Crafts",
+  //   "Concerts/Dance",
+  //   "Festival/Celebration",
+  //   "Lecture/Panel",
+  //   "Movies/Film",
+  //   "Play/Performance Art"
+  // ]
+  
+  // const LOCATIONS = []
+  // const LOCATIONS = [
+  //   "Miles Memorial Playhouse",
+  //   "Palisades Park",
+  //   "Annenberg Community Beach House"
+  // ]
+  const LOCATIONS = list.getAttribute("data-events-locations").split(",")
+  // const CONTACT_NAMES = list.getAttribute("data-events-contact-name").split(",")
+  
+  const CONTACT_EMAILS = []
+  // const CONTACT_EMAILS = [
+  //   "culture@smgov.net",
+  //   "naomi.okuyama@smgov.net"
+  // ]
+
+  const EVENT_TYPES = list.getAttribute("data-events-types") ? list.getAttribute("data-events-types").split(",") : []
+
+  // https://data.smgov.net/resource/tu9m-76aw.json?$where=contact_name = 'culture@smgov.net' OR contact_name = 'naomi.okuyama@smgov.net'
+  //https://data.smgov.net/resource/tu9m-76aw.json?$where=contact_emails = 'culture@smgov.net' OR contact_emails = 'naomi.okuyama@smgov.net'
+  function getWhereClause(columnName, items) {
+    let asStrings = items.map(item => `'${item}'`)
+    return `${columnName} = ${asStrings.join(` OR ${columnName} = `)}`
+  }
+
+  let eventTypesWhereClause = getWhereClause(`event_types`,   EVENT_TYPES)
+  let contactEmailsClause   = getWhereClause(`contact_email`, CONTACT_EMAILS)
+  let locationsClause       = getWhereClause(`location`,      LOCATIONS)
+
+  let whereClause
+  
+  if (LOCATIONS.length > 0 && EVENT_TYPES.length > 0) {
+    whereClause = `${locationsClause} OR (${eventTypesWhereClause})`
+  } else if (CONTACT_EMAILS.length > 0) {
+    whereClause = contactEmailsClause
+  } else if (LOCATIONS.length > 0) {
+    whereClause = locationsClause
+  } else if (EVENT_TYPES.length > 0) {
+    whereClause = eventTypesWhereClause
+  }
+
+  let url
+
+  if (whereClause) {
+    url = `https://data.smgov.net/resource/tu9m-76aw.json?$where=${encodeURIComponent(whereClause)}`
+  } else {
+    url = `https://data.smgov.net/resource/tu9m-76aw.json`
+  }
+
+  // SHIM: Avoid showing unrelated events from Annenberg Community Beach House
+  function isCulturalAffairsEvent(event) {
+    return (event.location     !== "Annenberg Community Beach House" || 
+            event.contact_name === "Beach=Culture")
+  }
+
+  console.log(url)
+
+  fetch(url)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(myJson) {
+      addItems(myJson)
+      // hideLoadingMessage()
+    })
+    .catch(function(error) {
+      console.error(error)
+      // showErrorMessage()
+    })
+
+  function addItems(data) {
+    // let template = document.getElementById('event-template')
+    let itemLimit = 999
+    if ( list.getAttribute("data-events-limit") && 
+         !isNaN(list.getAttribute("data-events-limit"))) {
+      itemLimit = Number(list.getAttribute("data-events-limit"))
+    }
+    let createdItems = 0
+    let itemLimitReached = false
+    if (data && list && template) {
+      data.sort(function(a, b) {
+        let aDate = new Date(a.start_date)
+        let bDate = new Date(b.start_date)
+        // a is less than b by some ordering criterion
+        if (aDate < bDate) {
+          return -1;
+        }
+        // a is greater than b by the ordering criterion
+        if (aDate > bDate) {
+          return 1;
+        }
+        // a must be equal to b
+        return 0;
+      })
+      for (let index = 0; index < data.length; index++) {
+        if (isCulturalAffairsEvent(data[index])) {
+          // console.log({ createdItems, itemLimit })
+          if (createdItems < itemLimit) {
+            let success = null
+            try {
+              success = createItem(data[index], list, template)
+            } catch(e) {
+              console.error(`Something went wrong while rendering an event using this data: `)
+              console.dir({ data: data[index] })
+            }
+            if (success) {
+              createdItems++
+            }
+          } else {
+            itemLimitReached = true
+          }
+        }
+      }
+      if (list.querySelectorAll("li").length <= 0) {
+        list.insertAdjacentHTML('beforebegin', "<p>More events are coming soon…</p>")
+      }
+    }
+
+    console.log({ itemLimitReached })
+
+    let more = document.querySelector("[data-events-more]")
+    if (itemLimitReached && more) {
+      more.style.visibility = "visible"
+    } else {
+      if (more) more.style.display = "none"
+    }
+
+    let fallback = document.querySelector("[data-events-fallback]")
+    if (fallback) fallback.parentNode.removeChild(fallback)
+  }
+
+  function paddedZeros(number) {
+    return (number < 10) ? `0${number}` : number
+  }
+
+  // KUDOS: https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
+  function getFormattedTime(date) {
+    let hours   = date.getHours()
+    let minutes = date.getMinutes()
+    let ampm    = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = (hours == 0) ? 12 : hours; // the hour '0' should be '12'
+    return `${hours}:${paddedZeros(minutes)} ${ampm}`
+  }
+
+  function createItem(itemData, list, template) {
+    const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let startDate = new Date(`${itemData.start_date}-07:00`)
+    let endDate   = new Date(`${itemData.end_date}-07:00`)
+
+    let startTime = getFormattedTime(startDate)
+    let endTime   = getFormattedTime(endDate)
+
+    let weekday     = daysOfTheWeek[startDate.getDay()]
+    let month       = monthNames[startDate.getMonth()]
+
+    let weekdayAbbreviation = weekday.substring(0, 3)
+    let monthAbbreviation   = month.substring(0, 3)
+
+    let year        = startDate.getFullYear();
+    let day         = startDate.getDate();
+    let title       = itemData.title
+    let time        = (startDate.getHours() == 0) ? `All day` : (endTime) ? `${startTime} to ${endTime}` : startTime
+    let location    = itemData.location
+    let address     = itemData.address_address
+    // let city        = itemData.address_city
+    // let state       = itemData.address_state
+    // let zip         = itemData.address_zip
+    let description = itemData.description
+    let categories  = itemData.event_types
+    let ages        = itemData.age_groups
+    let url         = itemData.detail_url
+    let urlDomain   = url.split("/")[2]
+    let dataCategories = itemData.event_types ? itemData.event_types.toLowerCase() : null
+    let dataDescription = itemData.description ? itemData.description.toLowerCase() : null
+
+    let className   = ""
+    if (location.toLowerCase().includes("palisades")) {
+      className = "palisades";
+    }
+    if (location.toLowerCase().includes("miles")) {
+      className = "miles";
+    }
+    if (location.toLowerCase().includes("beach")) {
+      className = "beach";
+    }
+
+    let html = template
+      .replace(/\{\{ weekday \}\}/g,     weekday)
+      .replace(/\{\{ month \}\}/g,       month)
+      .replace(/\{\{ year \}\}/g,        year)
+      .replace(/\{\{ weekdayAbbreviation \}\}/g, weekdayAbbreviation)
+      .replace(/\{\{ monthAbbreviation \}\}/g,   monthAbbreviation)
+      .replace(/\{\{ day \}\}/g,         day)
+      .replace(/\{\{ time \}\}/g,        time)
+      .replace(/\{\{ title \}\}/g,       title)
+      .replace(/\{\{ description \}\}/g, description)
+      .replace(/\{\{ location \}\}/g,    location)
+      .replace(/\{\{ address \}\}/g,    address)
+      .replace(/\{\{ url \}\}/g,         url)
+      .replace(/\{\{ urlDomain \}\}/g,  urlDomain)
+      .replace(/\{\{ className \}\}/g,  className)
+      .replace(/\{\{ dataCategories \}\}/g,  dataCategories)
+      .replace(/\{\{ dataDescription \}\}/g,  dataDescription)
+      
+  
+    if (categories) {
+      html = html.replace(/\{\{ categories \}\}/g, categories)
+    } else {
+      html = html.replace("<dt>Categories</dt>", "")
+      html = html.replace("<dd>{{ categories }}</dd>", "")
+    }
+
+    if (ages) {
+      html = html.replace(/\{\{ ages \}\}/g, ages)
+    } else {
+      html = html.replace("<dt>Ages</dt>", "")
+      html = html.replace("<dd>{{ ages }}</dd>", "")
+    }
+
+    // https://stackoverflow.com/questions/6963311/add-days-to-a-date-object
+    const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+    const THREE_DAYS = 3 * MILLISECONDS_PER_DAY;
+    let today = new Date();
+    let threeDaysAgo = new Date(today.setTime( today.getTime() - THREE_DAYS ));
+
+    // If the event is happening in the future or if it happened recently
+    if (startDate > threeDaysAgo) {
+      list.insertAdjacentHTML('beforeend', html)
+      return true
+    }
+
+    return false
+  }
+  function showLoadingMessage() {
+    document.body.classList.add('loading');
+    document.querySelector('.message').textContent = 'Loading data…'
+  }
+  function showErrorMessage() {
+    document.body.classList.add('error');
+    document.querySelector('.message').textContent = 'An error occurred while loading the data.'
+  }
+  function hideLoadingMessage() {
+    document.body.classList.remove('loading');
+    document.querySelector('.message').textContent = 'The data has finished loading.'
+  }
+})();
---
+++ assets/js/shim.js
@@ -0,0 +1,185 @@
+(function() {
+
+  // Link logo to santamonica.gov
+  let logo = document.querySelector("#layout-navigation .navbar .navbar-header .media a[href*='smgov.net']")
+  if (logo) {
+    logo.setAttribute("href", "https://santamonica.gov");
+    let logoImage = logo.querySelector("img");
+    if (logoImage) {
+      logoImage.setAttribute("alt", "Brought to you by the City of Santa Monica");
+    }
+  }
+
+  // Change the name of the site and remove the redundant home page link
+  let logoLink = document.querySelector("#layout-navigation .navbar .navbar-header .media .navbar-link.media-body.media-center")
+  if (logoLink) {
+    logoLink.textContent = "Santa Monica Cultural Affairs"
+    logoLink.removeAttribute("href")
+  }
+
+  // Move the main page heading after the image in the source order, for styling purposes
+  let pageTitle = document.querySelector(".page.content-item > header:first-child")
+  let primaryImage = document.querySelector(".page.content-item .image")
+  if (pageTitle && primaryImage) {
+    pageTitle.parentNode.removeChild(pageTitle)
+    primaryImage.parentNode.insertBefore(pageTitle, primaryImage.nextSibling)
+  }
+  
+  // Adjust the venue links on the home page
+  let images = document.querySelectorAll(".previewvenue div > img");
+  let links = document.querySelectorAll(".previewvenue a");
+  if (images.length && images.length > 0 && images.length === links.length) {
+    for (let index = 0; index < images.length; index++) {
+      let image = images[index]
+      let link = links[index]
+
+      // 1) Move the image so it’s inside the link
+      // (to make it easier to interact with the link)
+      if (image.parentNode.nodeName.toLowerCase() != "a") {
+        image.parentNode.removeChild(image)
+        link.insertBefore(image, link.firstChild)
+      }
+
+      // 2) Change the civic auditorium link so it points to an external web site
+      if (link.getAttribute("href").indexOf("civicauditorium") >= 0) {
+        link.setAttribute("href", "https://www.smgov.net/departments/ccs/civicauditorium/")
+      }
+      
+      // 3) Add icons
+      let paragraph = link.querySelector("p")
+      if (paragraph) {
+        if (link.getAttribute("href").includes("beach")) {
+          paragraph.insertAdjacentHTML("afterbegin", `<img src="/Media/arts/CA/icon-beachhouse.png" width="35" alt="" />`)
+        } else if (link.getAttribute("href").includes("civic")) {
+          paragraph.insertAdjacentHTML("afterbegin", `<img src="/Media/arts/CA/icon-civic-auditorium.png" width="35" alt="" />`)
+        } else if (link.getAttribute("href").includes("camera")) {
+          paragraph.insertAdjacentHTML("afterbegin", `<img src="/Media/arts/CA/icon-camera-obscura.png" width="35" alt="" />`)
+        } else if (link.getAttribute("href").includes("miles")) {
+          paragraph.insertAdjacentHTML("afterbegin", `<img src="/Media/arts/CA/icon-miles-playhouse.png" width="35" alt="" />`)
+        }
+      }
+    }
+  }
+
+  const firstPreviewVenue = document.querySelector(".previewvenue .col-md-6");
+  if (firstPreviewVenue) {
+    const civicHTML = `
+      <div class="col-md-6">
+        <div>
+          <a href="https://www.smgov.net/departments/ccs/civicauditorium/" target="_blank">
+            <img src="/Media/arts/CA/civicauditorium.jpg" alt="">
+            <p><img src="/Media/arts/CA/icon-civic-auditorium.png" width="35" alt="">The Civic Auditorium</p>
+          </a>
+        </div>
+      </div>
+      `
+    firstPreviewVenue.insertAdjacentHTML("afterend", civicHTML)
+  }
+  
+  // Remove the “attention artists” section from the home page
+  let artist = document.querySelector(".artist.jumbotron")
+  if (artist && document.documentElement.classList.contains("detail-home-page")) {
+    artist.parentNode.removeChild(artist)
+  }
+
+  
+  // Remove the policy section from the home page
+  let policy = document.querySelector(".zone-policy")
+  if (policy && document.documentElement.classList.contains("detail-home-page")) {
+    let policyRow = policy.parentNode.parentNode
+    policyRow.parentNode.removeChild(policyRow)
+  }
+
+
+  // Add a headline with a link above the "Upcoming Deadlines"
+  let upcoming = document.querySelector(".upcoming-details")
+  if (upcoming && document.documentElement.classList.contains("detail-home-page")) {
+    let secondaryHeadline = upcoming.querySelector("h1")
+    let headline = document.createElement("h1")
+    headline.innerHTML = `<a href="/arts/opportunities">Opportunities for Artists and Organizations</a>`
+    secondaryHeadline.parentNode.insertBefore(headline, secondaryHeadline)
+    
+    // Change the secondary headline to an “h2’
+    let newSecondaryHeadline = document.createElement("h2")
+    newSecondaryHeadline.textContent = secondaryHeadline.textContent
+    secondaryHeadline.parentNode.replaceChild(newSecondaryHeadline, secondaryHeadline)
+  }
+
+  
+  // Support HTML in featured artist section
+  let featuredArtist = document.querySelector(".featuredartist p")
+  if (featuredArtist && document.documentElement.classList.contains("detail-home-page")) {
+    let link = featuredArtist.querySelector("a")
+    link.parentNode.removeChild(link)
+    link.removeAttribute("target")
+    /*
+    if (link.getAttribute("href") === "/arts/go-see-art") {
+      link.setAttribute("href", "/arts/go-see-art#public-art")
+    }
+    */
+    featuredArtist.innerHTML = featuredArtist.textContent
+    featuredArtist.appendChild(link)
+  }
+
+
+  const featuredEventsHTML = `
+<a href="/arts/indigenous-now" class="featured-event">
+  <img src="/Media/arts/CA/2018_Tongva_Park_Jason_Abraham-(4-of-52).jpg" 
+       class="featured-event__image"
+       alt="" />
+  <div class="featured-event__text">
+    <h2 class="featured-event__title">Indigenous Now</h2>
+    <p class="featured-event__description">Saturday, May 11 from 12 – 4 p.m., Free admission</p>
+  </div>
+</a>
+<a href="/arts/meet-me-at-reed" class="featured-event">
+  <img src="/Media/arts/CA/2018_5_19_2018_Meet_Me_At_Reed_Taiko_Festival_Javier_Guillen (195 of 281).jpg" 
+       class="featured-event__image"
+       alt="" />
+  <div class="featured-event__text">
+    <h2 class="featured-event__title">Meet Me at Reed</h2>
+    <p class="featured-event__description">Saturdays: June 15 to July 27, 2019, Free admission</p>
+  </div>
+</a>
+<a href="/arts/art-in-the-parks" class="featured-event">
+  <img src="/Media/arts/CA/The%20Wonder%20Room%202015%20-%202%20William%20Short.jpg" 
+       class="featured-event__image"
+       alt="" />
+  <div class="featured-event__text">
+    <h2 class="featured-event__title">Art in the Parks</h2>
+    <p class="featured-event__description">Free Outdoor Performances to Look Forward to This Summer</p>
+  </div>
+</a>
+`
+/*
+<a class="featured-event-image-only" href="/arts/airport-artwalk">
+  <img src="/Media/arts/CA/art-walk.jpg"
+       alt="Santa Monica Airpot Artwalk 2019. Saturday, March 23 from 12-5pm. Rain or shine! Free admission" />
+</a>
+*/
+
+  // Add the home page features
+  let vision = document.querySelector(".vision")
+  if (vision && document.documentElement.classList.contains("detail-home-page")) {
+    vision.insertAdjacentHTML("afterend", featuredEventsHTML)
+  }
+
+  // Add the Go See Art features
+  let featuredEventsPlaceholder = document.querySelector(".featured-events-placeholder")
+  if (featuredEventsPlaceholder) {
+    featuredEventsPlaceholder.insertAdjacentHTML("afterend", featuredEventsHTML)
+  }
+
+  // Open external links and PDFs in a new window
+  document.querySelectorAll("a").forEach(link => {
+    let url = link.getAttribute("href")
+    if (url) {
+      if ((url.indexOf("http") === 0 && !url.includes(window.location.hostname)) ||
+           url.includes(".pdf")) {
+        link.setAttribute("target", "_blank")
+      }
+    }
+  })
+
+
+})()
