eff10c72fabdeef74f226ad0f346bee7260807d9
Jim Thoburn <jim@jimthoburn.com>
2019-07-25 18:14:19 -0700

    Show featured events from the “Go See Art” on the home page

--- assets/js/shim.js
+++ assets/js/shim.js
@@ -122,87 +122,50 @@
   }
 
 
-/*
-  const featuredEventsHTML = `
-<a href="/arts/meet-me-at-reed" class="featured-event">
-  <img src="/Media/arts/CA/2018_5_19_2018_Meet_Me_At_Reed_Taiko_Festival_Javier_Guillen (195 of 281).jpg" 
-       class="featured-event__image"
-       alt="" />
-  <div class="featured-event__text">
-    <h2 class="featured-event__title">Meet Me at Reed</h2>
-    <p class="featured-event__description">Saturdays: June 15 to July 27, 2019, Free admission</p>
-  </div>
-</a>
-<a href="/arts/jazz-2019" class="featured-event">
-  <img src="/Media/arts/CA/JOTL-Banner.jpg" 
-       class="featured-event__image"
-       alt="" />
-  <div class="featured-event__text">
-    <h2 class="featured-event__title">Jazz on the Lawn</h2>
-    <p class="featured-event__description">Sundays in August, Free admission</p>
-  </div>
-</a>
-<a href="/arts/art-in-the-parks" class="featured-event">
-  <img src="/Media/arts/CA/The%20Wonder%20Room%202015%20-%202%20William%20Short.jpg" 
-       class="featured-event__image"
-       alt="" />
-  <div class="featured-event__text">
-    <h2 class="featured-event__title">Art in the Parks</h2>
-    <p class="featured-event__description">Free Outdoor Performances to Look Forward to This Summer</p>
-  </div>
-</a>
-`
-*/
+  // Add features to the home page
+  let vision = document.querySelector(".vision")
+  if (vision && document.documentElement.classList.contains("detail-home-page")) {
+    let featuredEventsHTML = null;
+    fetch("/arts/go-see-art")
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(responseText) {
+        console.log(responseText);
+        let start   = responseText.indexOf('<ol id="featured-events" class="featured-event-list">');
+        let end     = responseText.indexOf('</ol>');
+        if (start >= 0 && end >= 0) {
+          featuredEventsHTML = responseText.substring(start, end);
+          vision.insertAdjacentHTML("afterend", featuredEventsHTML)
 
-/*
-<a href="/arts/beach-dances" class="featured-event">
-<img src="https://www.santamonica.gov/Media/arts/CA/2018_6_7_18_SarahElgart_BeachDances_BH_Sam%20McReynolds_Genna%20Moroni_%20Jonathan%20Stanley_%20Alexandria%20Diaz%20de%20Fato_photobyJasonAbraham2.jpg" 
-     class="featured-event__image"
-     alt="" />
-<div class="featured-event__text">
-  <h2 class="featured-event__title">Beach Dances</h2>
-  <p class="featured-event__description">June 18 – 23, 2019, Free admission</p>
-</div>
-</a>
-<a href="/arts/indigenous-now" class="featured-event">
-  <img src="/Media/arts/CA/2018_Tongva_Park_Jason_Abraham-(4-of-52).jpg" 
-       class="featured-event__image"
-       alt="" />
-  <div class="featured-event__text">
-    <h2 class="featured-event__title">Indigenous Now</h2>
-    <p class="featured-event__description">Saturday, May 11 from 12 – 4 p.m., Free admission</p>
-  </div>
-</a>
-<a class="featured-event-image-only" href="/arts/airport-artwalk">
-  <img src="/Media/arts/CA/art-walk.jpg"
-       alt="Santa Monica Airpot Artwalk 2019. Saturday, March 23 from 12-5pm. Rain or shine! Free admission" />
-</a>
-*/
+          // Update the features HTML
+          let featuredEvents = document.querySelectorAll("#featured-events li")
+          featuredEvents.forEach(item => {
+            let link = item.querySelector("a")
+            let text = item.querySelector(".featured-event__text")
+            if (link && text) {
+              text.parentNode.removeChild(text)
+              link.appendChild(text)
+            }
+          })
+        }
+      });
 
-  if (featuredEventsHTML) {
-
-    // Add the home page features
-    let vision = document.querySelector(".vision")
-    if (vision && document.documentElement.classList.contains("detail-home-page")) {
-      vision.insertAdjacentHTML("afterend", featuredEventsHTML)
-    }
-
-    // Add the Go See Art features
-    let featuredEventsPlaceholder = document.querySelector(".featured-events-placeholder")
-    if (featuredEventsPlaceholder) {
-      featuredEventsPlaceholder.insertAdjacentHTML("afterend", featuredEventsHTML)
-    }
+  // For the “Go See Art” page
+  } else {
+    
+    // Update the features HTML
+    let featuredEvents = document.querySelectorAll("#featured-events li")
+    featuredEvents.forEach(item => {
+      let link = item.querySelector("a")
+      let text = item.querySelector(".featured-event__text")
+      if (link && text) {
+        text.parentNode.removeChild(text)
+        link.appendChild(text)
+      }
+    })
   }
 
-  let featuredEvents = document.querySelectorAll("#featured-events li")
-  featuredEvents.forEach(item => {
-    let link = item.querySelector("a")
-    let text = item.querySelector(".featured-event__text")
-    if (link && text) {
-      text.parentNode.removeChild(text)
-      link.appendChild(text)
-    }
-  })
 
   // Open external links and PDFs in a new window
   document.querySelectorAll("a").forEach(link => {
